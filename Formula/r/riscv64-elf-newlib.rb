4b9e273ee7473f4527410e20c428a341f2c8093b
Ryar Nyah <ryarnyah@gmail.com>
2025-02-12 01:59:41 +0100

    riscv64-elf-newlib 4.5.0 (new formula)

--- Formula/r/riscv64-elf-newlib.rb
+++ Formula/r/riscv64-elf-newlib.rb
@@ -2,23 +2,22 @@
 #                https://rubydoc.brew.sh/Formula
 # PLEASE REMOVE ALL GENERATED COMMENTS BEFORE SUBMITTING YOUR PULL REQUEST!
 class Riscv32ElfNewlib < Formula
-  desc "A C standard library implementation intended for use on embedded systems (RISCV32 bare metal)"
+  desc "A C standard library implementation intended for use on embedded systems (RISCV64 bare metal)"
   homepage "https://www.sourceware.org/newlib/"
   url "ftp://sourceware.org/pub/newlib/newlib-4.5.0.20241231.tar.gz"
   sha256 "33f12605e0054965996c25c1382b3e463b0af91799001f5bb8c0630f2ec8c852"
   license "BSD"
 
-  depends_on "riscv32-elf-binutils" => :build
-  depends_on "riscv32-elf-gcc" => :build
+  depends_on "riscv64-elf-binutils" => :build
+  depends_on "riscv64-elf-gcc" => :build
 
   on_system :linux, macos: :ventura_or_newer do
     depends_on "texinfo" => :build
   end
 
   def install
-    target = "riscv32-elf"
+    target = "riscv64-elf"
     args = %W[
-      --prefix=#{prefix}
       --target=#{target}
       --infodir=#{info}
       --mandir=#{man}
@@ -39,14 +38,15 @@ class Riscv32ElfNewlib < Formula
   end
 
   test do
-    (testpath/"test.c").write <<~C
-      #include <stdint.h>
-
-      int main() {
-        int8_t i = 0;
-        return 0;
-      }
-    C
-    system Formula["riscv32-elf-gcc"].bin/"riscv32-elf-gcc", "-I#{include}", "-L#{lib}", "-c", "-o", testpath/"test-c.o", testpath/"test.c"
+    # `test do` will create, run in and delete a temporary directory.
+    #
+    # This test will fail and we won't accept that! For Homebrew/homebrew-core
+    # this will need to be a test that verifies the functionality of the
+    # software. Run the test with `brew test riscv32-elf-newlib`. Options passed
+    # to `brew install` such as `--HEAD` also need to be provided to `brew test`.
+    #
+    # The installed folder is not in the path, so use the entire path to any
+    # executables being tested: `system bin/"program", "do", "something"`.
+    system "false"
   end
 end
